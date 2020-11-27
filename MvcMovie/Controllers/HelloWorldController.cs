8bc26d33c4aece46065445d3b993a009ff4cbfa2
adrian <vanchaos@gmail.com>
2020-11-27 16:12:33 +1000

    Add View

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,26 +8,18 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        //// GET: HelleWorld
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
-
-        // 
-        // GET: /HelloWorld/ 
-
-        public string Index()
+        // GET: HelleWorld
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
     }
 }
